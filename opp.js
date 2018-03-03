a8e728da8537c81357e840627e41b2cf8434c3c1
beng02 <32522595+beng02@users.noreply.github.com>
2018-03-03 11:15:22 +0000

    Update and rename _config.yml to opp.js

---
+++ opp.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var hid = $("#tie-block_1042, .logo_normal, .logo_2x, a[rel=noopener], .share-links, .author-name, .text-html-box, p:contains('QIURA'), p:contains('MOVIEU'), p:contains('FACEBOOK'), p:contains('====='), p:contains('media sosial'), p:contains('Samehada'), p:contains('Penerjemah'), p:contains('MANGAKYO'), ins, #disqus_thread, strong:contains('RiiE.net')");
+
+hid.remove();
+hid.css("display", "none !important");
+hid.empty();
+hid.hide();
+
+$("title:eq(0)").text( $("title:eq(1)") );
+
+var lik = $("div.download-link").html();
+$("#link").html("<br /><center style='font-weight:bold;font-size:large;text-color:red;'>"+lik+"</center>");
+$("#telo").remove();
+
+});
