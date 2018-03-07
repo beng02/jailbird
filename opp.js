a613664dda555747004df2df89fc951ccb6cff73
mazbeng <32522595+mazbeng@users.noreply.github.com>
2018-03-07 12:34:02 +0700

    Update opp.js

--- opp.js
+++ opp.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var hid = $("#tie-block_1042, .logo_normal, .logo_2x, a[rel=noopener], .share-links, .author-name, .text-html-box, p:contains('QIURA'), p:contains('MOVIEU'), p:contains('FACEBOOK'), p:contains('====='), p:contains('media sosial'), p:contains('Samehada'), p:contains('Penerjemah'), p:contains('MANGAKYO'), ins, #disqus_thread, p:contains('RiiE.net')");
+var hid = $(".logo_normal, .logo_2x, a[rel=noopener], .share-links, .author-name, .text-html-box, p:contains('QIURA'), p:contains('MOVIEU'), p:contains('FACEBOOK'), p:contains('====='), p:contains('media sosial'), p:contains('Samehada'), p:contains('Penerjemah'), p:contains('MANGAKYO'), ins, #disqus_thread, p:contains('RiiE.net')");
 
 hid.remove();
 hid.css("display", "none !important");
