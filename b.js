83d7272c3917aa7705539f419c21792a833d58de
mazbeng <32522595+mazbeng@users.noreply.github.com>
2018-03-14 03:29:58 +0000

    Update b.js

--- b.js
+++ b.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-var jj = $("a.tex").length;
-    for (var i = 0; i < jj; i++) {
+var jj = $("a.unduh");
+var jjj = $("b.unduh");
+  if ( jj == $("a.unduh")){
+    for (var i = 0; i < jj.length; i++) {
      var a = $("a.tex").eq( i ).attr("href");
      var enca = btoa( a );
      $("a.tex").eq( i ).html("<a rel='nofollow' href='http://jail-bird.blogspot.co.id/p/link.html#" + enca + "'><input type='button' style='color:red;text-color:white;margin:3px;padding:3px;' value='INI LINKNYA' /></a>");
     }
-var jjj = $("b.unduh").length;
-    for (var u = 0; u < jjj; u++) {
+      } else if ( jjj = $("b.unduh")){
+    for (var u = 0; u < jjj.length; u++) {
      var b = $("b.unduh").eq( u ).text();
      var encb = btoa( b );
      $("b.unduh").eq( u ).html("<a rel='nofollow' href='http://jail-bird.blogspot.co.id/p/link.html#" + encb + "'><input type='button' style='color:red;text-color:white;margin:3px;padding:3px;' value='INI LINKNYA' /></a>");
     }
+      }
     
 
 if( $(".link").text() == "hhh" ) {
